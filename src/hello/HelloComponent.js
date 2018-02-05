df02996f4f3e9000c81607daeeef868bc3f30f93
William Walker <wwalke@a1707-14041.icims.net>
2018-02-05 17:22:17 -0500

    integrating redux form

--- src/hello/HelloComponent.js
+++ src/hello/HelloComponent.js
@@ -4,19 +4,23 @@ import { Field, reduxForm } from 'redux-form';
 
 const log = require('simple-console-logger').getLogger('HelloComponent');
 
-const buttonStyle={padding:'1em', margin: '1em'};
-const inputStyle={padding: '1em', margin: '1em'};
-
+/**
+ * This is how Redux form Field components allow you to render
+ * any custom input component. The "input" props should all be
+ * passed to the <input> element. You can pass in 
+ */
 const renderField = ({
   input,
   label,
   type,
+  inputStyle,
+  labelStyle,
   meta: { touched, error, warning }
 }) => (
   <div>
-    <label>{label}</label>
+    <label style={labelStyle}>{label}</label>
     <div>
-      <input {...input} placeholder={label} type={type} />
+      <input {...input} placeholder={label} type={type} style={inputStyle}/>
       {touched && ((error && <span>{error}</span>))}
     </div>
   </div>
@@ -46,26 +50,38 @@ class HelloComponent extends React.Component {
   }
 
   hello = () => {
+    console.log('hello')
     this.props.sayHello(this.state.message);
   }
   helloAsync = () => {
+    console.log('hello async')
     this.props.sayHelloAsync(this.state.message);
   }
   changeMessage = (e) => {
     this.setState({message: e.target.value});
   }
 
+  onSubmit = (values) => {
+    console.log('submit', values);
+  }
+
   render() {    
     const { handleSubmit, pristine, reset, submitting } = this.props
+
+    const buttonStyle={padding:'1em', margin: '1em'};
+    const inputStyle={padding: '1em', margin: '1em'};
+    const labelStyle={marginLeft: '1em'};
+        
     return (
       <div style={{padding:'2em'}}>
-        <form onSubmit={handleSubmit}>
-          <input 
-            value={this.state.message}
-            onChange={this.changeMessage}
-            type="text" 
-            placeholder="Enter message"
-            style={inputStyle} 
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field
+            inputStyle={inputStyle}
+            labelStyle={labelStyle}
+            name="message"
+            type="text"
+            label="Message"
+            component={renderField}
           />
           <br/>
           <button style={buttonStyle} disabled={submitting} onClick={this.hello}>Say Hello</button>
