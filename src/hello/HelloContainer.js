0dcb87cd9694486b1d006a6e6a2aa86697fc18e9
William Walker <wwalke@a1707-14041.icims.net>
2018-02-06 08:49:09 -0500

    formatting

--- src/hello/HelloContainer.js
+++ src/hello/HelloContainer.js
@@ -6,12 +6,14 @@ import { sayHello, sayHelloAsync } from './hello';
 
 class HelloContainer extends React.Component {
   render() {
-    return <HelloComponent 
-      message={this.props.message} 
-      sayHello={this.props.sayHello}
-      sayHelloAsync={this.props.sayHelloAsync}
-      pending={this.props.pending}
-    />
+    return (
+      <HelloComponent 
+        message={this.props.message} 
+        sayHello={this.props.sayHello}
+        sayHelloAsync={this.props.sayHelloAsync}
+        pending={this.props.pending}
+      />
+    );
   }
 }
 
